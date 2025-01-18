0b4d7c9139b96141bd58f91effe15f63246ad609
ChristKTMB <christkatumba02@gmail.com>
2025-01-18 14:36:05 +0100

    Ajout de la gestion des routes pour les monstres, amélioration du composant Monster avec des signaux et ajout d'un bouton pour naviguer vers le monstre suivant. route en details avec de navigation de pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MonsterListComponent } from './pages/monster-list/monster-list.component';
 import { MonsterComponent } from './pages/monster/monster.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [{
     path: "",
@@ -11,5 +12,14 @@ export const routes: Routes = [{
     component: MonsterListComponent
 },{
     path: "monster",
-    component: MonsterComponent
+    children: [{
+        path: "",
+        component: MonsterComponent
+    },{
+        path: ":id",
+        component: MonsterComponent
+    }]
+},{
+    path: '**',
+    component: NotFoundComponent
 }];
