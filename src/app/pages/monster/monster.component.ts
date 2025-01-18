af8858fbcfb6e888855f75994d1e5613c51befbf
ChristKTMB <christkatumba02@gmail.com>
2025-01-18 15:41:14 +0100

    Ajout d'un formulaire FORMCONTROL pour la création de monstres avec validation des champs et gestion des erreurs.

--- src/app/pages/monster/monster.component.ts
+++ src/app/pages/monster/monster.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, OnDestroy, OnInit, signal } from '@angular/core';
+import { FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-monster',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './monster.component.html',
   styleUrl: './monster.component.css'
 })
@@ -15,6 +16,8 @@ export class MonsterComponent implements OnInit, OnDestroy {
 
   monsterId = signal<number | undefined>(undefined);
   routeSubscription: Subscription | null = null;
+  name = new FormControl('', [Validators.required]);
+  hp = new FormControl(0, [Validators.required, Validators.min(1), Validators.max(200)]);
 
   ngOnInit(): void {
     this.routeSubscription = this.route.params.subscribe(params => {
@@ -31,4 +34,12 @@ export class MonsterComponent implements OnInit, OnDestroy {
     nextId++;
     this.router.navigate(['/monster/' + nextId]);
   }
+
+  onSubmit(event: Event){
+    event.preventDefault();
+    console.log('Name:', this.name.value);
+    console.log(this.hp.value);
+
+  }
+
 }
